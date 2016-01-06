0c26c133a4f9dccc46565e74d6395983294ed431
Debora Duarte <deboraaline@gmail.com>
2016-01-06 18:03:24 -0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to centrality-service
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/centrality_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns centrality-service.core-test
+  (:require [clojure.test :refer :all]
+            [centrality-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
