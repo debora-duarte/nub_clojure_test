693f24b1a1b1a8ab7e80999a9539c5c7b148cb8b
Debora Duarte <deboraaline@gmail.com>
2016-01-08 11:34:55 -0200

    Implements single-source shortest paths calculation using breadth-first search

--- src/centrality_service/graph.clj
+++ src/centrality_service/graph.clj
@@ -14,3 +14,31 @@
 
 (defn build-graph [graph edges]
   (reduce add-edge graph edges))
+
+(defn adjacent-vertices-not-marked [shortest vertice graph]
+  (filter #(= (shortest %) nil) (graph vertice))) 
+
+(defn recalculate-queue [queue new-vertices]
+  (concat (rest queue) new-vertices))
+
+(defn recalculate-shortest [shortest current-vertice adj-vertices]
+  (let [dist-current-vertice (shortest current-vertice)]
+    (into (vector) 
+      (map-indexed 
+        (fn [index item] 
+          (if (some #(= % index) adj-vertices) (inc dist-current-vertice) item))  
+        shortest))))
+
+(defn initialize-shortest [graph vertice]
+  (assoc (vec (repeat (count graph) nil)) vertice 0))
+
+(defn shortest-path [graph vertice]
+  "Calculates the single-source shortest paths using Breadth-first search algorithm"
+  (loop [queue (list vertice)
+         shortest (initialize-shortest graph vertice)]
+    (if (empty? queue)
+      shortest
+      (let[current-vertice (first queue)
+           ajd-vertices (adjacent-vertices-not-marked shortest current-vertice graph)]
+        (recur  (recalculate-queue queue ajd-vertices)  
+                (recalculate-shortest shortest current-vertice ajd-vertices))))))
--- test/centrality_service/graph_test.clj
+++ test/centrality_service/graph_test.clj
@@ -4,28 +4,38 @@
 
 (deftest graph-construction
   (testing "has-d-egdge?"
-    (let [graph [#{1} #{2, 3}]]
+    (let [graph {0 #{1} 1 #{2, 3}}]
       (is (true? (has-d-edge? graph 0 1)) "Does not assert the presence of the 0->1 edge properly")))
 
   (testing "has-egdge?"
-    (let [graph [#{1} #{0, 2}]]
+    (let [graph {0 #{1} 1 #{0, 2}}]
       (is (true? (has-edge? graph 0 1)) "Does not assert the presence of the 0-1 edge properly")
       (is (true? (has-edge? graph 1 0)) "Does not assert the presence of the 0-1 edge properly")))
 
   (testing "add-d-edge"
-    (let [graph (add-d-edge [#{}] '(0 1))]
+    (let [graph (add-d-edge {} '(0 1))]
       (is (true? (has-d-edge? graph 0 1)) "Does not add the 0->1 add to the graph")))
 
   (testing "add-edge"
-    (let [graph (add-edge [#{} #{}] '(0 1))]
+    (let [graph (add-edge {} '(0 1))]
       (is (true? (has-edge? graph 0 1)) "Does not add the 0-1 add to the graph")
       (is (true? (has-edge? graph 1 0)) "Does not add the 0-1 add to the graph")))
 
+  (def edges ['(6 4) '(4 3) '(4 5) '(3 2) '(5 2) '(5 1) '(2 1) '(1 0)])
+
   (testing "build-graph"
-    (let [edges ['(6 4) '(4 3) '(4 5) '(3 2) '(5 2) '(5 1) '(2 1) '(1 0)]
-          graph (build-graph [#{} #{} #{} #{} #{} #{} #{}] edges)]
+    (let [graph (build-graph {} edges)]
       (is (true? (every? (fn [[v1 v2]] (has-edge? graph v1 v2)) edges)) 
         (str "Does not add all the edges to the graph:" graph))))
-  )
+
+  (testing "shortest-path"
+    (let [expected-shotest-path-to-0 [0 1 2 3 3 2 4]
+          graph (build-graph {} edges)
+          shortest-path-to-0 (shortest-path graph 0)]
+      (is (= 0 (compare expected-shotest-path-to-0 shortest-path-to-0))
+        (str  "Does not calculate the shortes-path correctly:" shortest-path-to-0 "\n"
+              "Expected: " expected-shotest-path-to-0)))))
+
+
 
 
