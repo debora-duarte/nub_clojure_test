eb93e24e73de13368d1109c03b59e1d4f3280224
Debora Duarte <deboraaline@gmail.com>
2016-01-11 00:49:50 -0200

    Changes defs in core to refs to deal better with concurrent calls

--- src/centrality_service/core.clj
+++ src/centrality_service/core.clj
@@ -6,62 +6,56 @@
   (:require [centrality-service.score :as score]))
 
 (def edges-file "edges.txt")
-(def ^:dynamic *fraudulent-clients* #{})
-
-; Not all these bindings should be here.
-; #TODO If there's time left, refactor to move some of them and to encapsulate some of the complexity,
-; especially the distances calculation
-(defn set-graph [graph] (def ^:dynamic *graph* graph))
-; The whole point of functional programming is not keeping state and having functions without side-effects.
-; But in this case, keeping the graph and, especially, the distances makes the program a lot faster particularly
-; in the case of big graphs
-(defn set-distances [distances] (def ^:dynamic *distances* distances))
-(defn set-scores [scores] (def ^:dynamic *scores* scores))
-(defn add-fraudulent-client [client]
-  (def ^:dynamic *fraudulent-clients* (conj *fraudulent-clients* client)))
 
 (defn initialize [] 
   (let [edges (util-ns/load-edges edges-file)]
-    (set-graph (graph-ns/build-graph {} edges))
-    (set-distances (shortest-path/all-pairs-shortest-path *graph*))
-    (set-scores (score/rank (score/closeness *distances*)))))
+    ; Not all these bindings should be here.
+    ; #TODO If there's time left, refactor to move some of them and to encapsulate some of the complexity,
+    ; especially the distances calculation
+    (def graph (ref (graph-ns/build-graph {} edges)))
+    (def distances (ref (shortest-path/all-pairs-shortest-path @graph)))
+    (def scores (ref (score/rank (score/closeness @distances))))
+    (def fraudulent-clients (ref #{}))))
 
 (defn rank-the-clients []
-  (score/rank *scores*))
+  (score/rank @scores))
     
 (defn add-another-edge [edge]
-  (set-graph (graph-ns/add-edge *graph* edge))
-  (set-distances (shortest-path/all-pairs-shortest-path *graph*))
-  (set-scores (score/rank (score/closeness *distances*)))
-  (set-scores (score/rank (score/mark-as-fraudulents *distances* *scores* *fraudulent-clients*))))
+  (dosync
+    (alter graph graph-ns/add-edge edge)
+    (ref-set distances (shortest-path/all-pairs-shortest-path @graph))
+    (ref-set scores (score/closeness @distances))
+    (alter scores (partial score/mark-as-fraudulents @distances) @fraudulent-clients)))
 
-(defn mark-as-fraudulent [client] ()
-  (add-fraudulent-client client)
-  (set-scores (score/rank (score/mark-as-fraudulent *distances* *scores* client))))
+(defn mark-as-fraudulent [client]
+  (dosync
+    (commute fraudulent-clients conj client) ; adds to the list of fraudulent clients
+    (alter scores (partial score/mark-as-fraudulent @distances) client))) ; recalculates score
 
 ; If there's time left, make a real test - an automatic one - out of it
 (defn -main
   [& args]
   (println "---------------------------after initialize--------------------------------------")
   (initialize)
-  (println *scores*)
+  (println @graph)
+  (println (rank-the-clients))
   (println "---------------------------add-edge old 64-48------------------------------------")
   (add-another-edge '(64 48))
-  (println *scores*)
+  (println (rank-the-clients))
   (println "---------------------------add-edge new 88-33------------------------------------")
   (add-another-edge '(88 33))
-  (println *scores*)
+  (println (rank-the-clients))
   (println "---------------------------mark 44 as fraudulent---------------------------------")
   (mark-as-fraudulent 44)
-  (println *scores*)
+  (println (rank-the-clients))
   (println "---------------------------mark 88 as fraudulent---------------------------------")
   (mark-as-fraudulent 88)
-  (println *scores*)
+  (println (rank-the-clients))
   (println "---------------------------add-edge new 5-51-------------------------------------")
   (add-another-edge '(5 51))
   (add-another-edge '(5 76))
   (add-another-edge '(5 89))
-  (println *scores*)
+  (println (rank-the-clients))
   (println "---------------------------rank-the-clients--------------------------------------")
   (println (rank-the-clients)))
 
